c95029eb254b1239102505457452f6d06ec57fef
Wesley Moraes <wesleycrz@hotmail.com>
2025-08-04 17:04:53 -0300

    atualizando o vite para hospedagem

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,29 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
+export default defineConfig(({ mode }) => ({
+  base: "/acolhendo-a-sombra-site/",  
+  server: {
+    host: "::",
+    port: 8080,
+  },
+  plugins: [
+    react(),
+    mode === "development" && componentTagger(),
+  ].filter(Boolean),
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+}));
+
+/*
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import path from "path";
+import { componentTagger } from "lovable-tagger";
+
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   server: {
@@ -20,3 +43,4 @@ export default defineConfig(({ mode }) => ({
     },
   },
 }));
+*/
