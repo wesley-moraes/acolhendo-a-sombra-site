e5bc81bbc7cf02551f73793b9c540bffae33c450
Wesley Moraes <wesleycrz@hotmail.com>
2025-08-04 17:31:33 -0300

    update vite

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 import { componentTagger } from "lovable-tagger";
 
 export default defineConfig(({ mode }) => ({
-  base: "/acolhendo-a-sombra-site/",  
+  base: "/",  
   server: {
     host: "::",
     port: 8080,
